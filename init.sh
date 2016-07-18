9b902369b32b1dec102257f89ee1758e569d4afa
Mario Pareja <pareja.mario@gmail.com>
2016-07-18 12:28:17 -0400

    init: generate directory for defining a Tarantino world.

---
+++ init.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+init() {
+	git init
+
+	cp $TT_SHARE/templates/docker-compose.yml .
+	echo sample docker-compose.yml file created
+
+	cp -R $TT_SHARE/templates/grafana .
+}
+
+tt_init() {
+	if [ -f docker-compose.yml ]; then
+		local warning='Directory already has files, are you sure (y/N)? '
+		read -n 1 -p "$warning" CONFIRM
+		if [ "$CONFIRM" = 'y' ]; then
+			echo
+			init
+		fi
+	else
+		init
+	fi
+}
+
