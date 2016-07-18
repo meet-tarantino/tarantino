048b99c7321fc21d1142f49816a402e7a5e1ec33
Mario Pareja <pareja.mario@gmail.com>
2016-07-18 13:47:41 -0400

    Add workspace menu and usage.

--- workspace.sh
+++ workspace.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
 
-tt_init() {
+tt_workspace() {
+	main ws $*
+}
+
+ws_usage() {
+	tt_header
+	echo 'usage: tt workspace <command> [args]'
+	echo
+	echo commands:
+	echo
+	echo '  init - initialize a new workspace in the current directory'
+	echo '  add <workspace_dir> - register an existing workspace with your user account'
+	echo '  rm <workspace> - unregister a workspace from your user account'
+	echo '  ls - list registered workspaces '
+	echo '  use <workspace> - switch to a different workspace'
+	echo '  upgrade [workspace] - fetch latest workspace definition, update docker images'
+}
+
+ws_init() {
 	if [ -f docker-compose.yml ]; then
 		local warning='Directory already has files, are you sure (y/N)? '
 		read -n 1 -p "$warning" CONFIRM
