ba6477aeb7c4a216885c74f2db84a777af225ea4
Mario Pareja <pareja.mario@gmail.com>
2016-07-19 10:39:13 -0400

    Move sample-data scripts to plugin sample.

---
+++ templates/plugins/data.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+tt_data() {
+	local script=$1
+	shift
+
+	if [ -z "$script" ]; then
+		ttd_usage
+		return 1;
+	fi
+
+	run_sample_data $script $@
+}
+
+ttd_usage() {
+	local sample_scripts=$(tt_sample_data_scripts)
+
+	tt_header
+	echo 'usage: tt data <command> [args]'
+	echo
+	echo commands:
+	echo
+	echo '  <command> - runs the sample-data project npm script of the same name'
+	if [ ! -z "$sample_scripts" ]; then
+		echo '    available commands:'
+		for script in $sample_scripts; do
+			echo "      $script"
+		done
+	fi
+}
+
+run_sample_data() {
+	local sample_data_project="$TT_PROJECTS/$SAMPLE_DATA"
+
+	local script=$1
+	shift
+
+	if [ ! -d $sample_data_project ]; then
+		echo "requires the $SAMPLE_DATA repository, cloning..."
+		tt_clone $SAMPLE_DATA
+	fi
+
+	echo "running sample data script: $script"
+
+	if [ $# -gt 0 ]; then
+		script="$script -- $@" # add any extra args
+	fi
+
+	pushd $sample_data_project;
+
+	npm run $script
+	result=$?
+
+	popd &> /dev/null
+	return $result
+}
+
