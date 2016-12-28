c66c133ec920479739040377e033e089069aefef
Mario Pareja <pareja.mario@gmail.com>
2016-12-28 16:40:01 -0500

    support specifying source control repository for a service
      + parses docker-compose.yml to allow adding a `com.tarantino.source` label to docker-compose services
      + does not yet do anything with the value specified, but anticipating using an npm-like algorithm for various sources
      + begin renaming dc_get_all_services and dc_get_services to something more explicit

--- dc.sh
+++ dc.sh
@@ -21,43 +21,53 @@ dc_get_links() {
 	done
 }
 
+# TODO: remove this once references are updated to dc_get_services
 dc_get_all_services() {
-	if [ "$TT_IS_GLOBAL" = true ]; then
-		generate_service_lists
-		cat "$(get_workspace_cache_dir)/services"
-	else
-		dc config --services
-	fi
+	get_services
 }
 
+# TODO: remove this once references are updated to dc_get_services_with_source
 dc_get_services() {
-	dc_get_all_services | grep -Ev $NOT_SERVICES_PATTERN
+	dc_get_services_with_source
 }
 
+dc_get_services_with_source() {
+	# pluck out service names only
+	dc_get_sources | sed -e 's/^\(.*\)=.*$/\1/'
+}
+
+# TODO: remove this once references are updated to dc_get_service_and_dependencies_with_source
 dc_get_repos() {
+	dc_get_service_and_dependencies_with_source $@
+}
+
+dc_get_service_and_dependencies_with_source() {
+	# intersect service & dependencies with source backed services
+	sort <(dc_get_service_and_dependencies $@) <(dc_get_services_with_source) | uniq -d
+}
+
+dc_get_service_and_dependencies() {
 	if [[ $# -gt 0 ]]; then
 		services=
-		repos=$@
-		while [ "$services" != "$repos" ]; do
-			services=$repos
-			repos=
+		result=$@ # start with the ones passed in
+		while [ "$services" != "$result" ]; do
+			services=$result
+			result=
+
+			# for each service...
 			for service in $services; do
-				self=$(echo $service | grep -Ev $NOT_SERVICES_PATTERN)
-				if [ ! -z "$self" ]; then
-					repos="$repos $self"
-				fi
-				links=$(dc_get_links $service | grep -Ev $NOT_SERVICES_PATTERN)
-				if [ ! -z "$links" ]; then
-					repos="$repos $links"
-				fi
+				# add service and linked services to result
+				result="$result $service $(dc_get_links $service)"
 			done
-			repos=$(echo $repos | awk -v RS="[\n ]" '!a[$0]++')
+
+			# remove duplicates and normalize whitespace
+			result=$(echo $result | awk -v RS="[\n ]" '!a[$0]++')
 		done
-		if [ ! -z "$repos" ]; then
-			echo "$repos"
+		if [ ! -z "$result" ]; then
+			echo "$result"
 		fi
 	else
-		echo "$(tt_get_services)"
+		echo "$(get_services)"
 	fi
 }
 
@@ -67,7 +77,26 @@ get_workspace_cache_dir() {
 	echo $cachedir
 }
 
-generate_service_lists() {
+# TODO: rename this to dc_get_services
+get_services() {
+	if [ "$TT_IS_GLOBAL" = true ]; then
+		generate_compose_file_caches
+		cat "$(get_workspace_cache_dir)/services"
+	else
+		dc config --services
+	fi
+}
+
+dc_get_sources() {
+	if [ "$TT_IS_GLOBAL" = true ]; then
+		generate_compose_file_caches
+		cat "$(get_workspace_cache_dir)/sources"
+	else
+		parse_sources
+	fi
+}
+
+generate_compose_file_caches() {
 	local md5check=$(get_workspace_cache_dir)/dc_file.md5
 
 	if [ -f "$md5check" ]; then
@@ -79,5 +108,14 @@ generate_service_lists() {
 	local services_cache_file=$(get_workspace_cache_dir)/services
 	dc config --services > "$services_cache_file"
 
+	local sources_cache_file=$(get_workspace_cache_dir)/sources
+	parse_sources > $sources_cache_file
+
 	md5sum "$(dc_file)" > "$md5check"
+}
+
+parse_sources() {
+	parse_yaml $(dc_file) |
+		grep '^services_.*_labels_com.tarantino.source=' |
+		sed -e 's/^services_\(.*\)_labels_com.tarantino.source=/\1=/'
 }
