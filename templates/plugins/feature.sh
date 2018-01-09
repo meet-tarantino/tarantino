50d9bf3729be44cb16093e5c75a53405a364147e
Darren McElligott <darren.mcelligott@gmail.com>
2018-01-09 14:07:35 -0500

    Added workspace plugin usage commands seam

---
+++ templates/plugins/feature.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+## Sample tarantino plugin
+# tt_myfeature() {
+#	echo "My feature is super cool..."
+# }
+
+## Sample usage extension function (must end in 'plugin_usage')
+# myfeature_plugin_usage() {
+#	echo "  myfeature - outputs a special message"
+# }
