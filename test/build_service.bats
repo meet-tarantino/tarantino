8569b19560ec4869c8182117ebd318a530cf65ce
Mario Pareja <pareja.mario@gmail.com>
2018-11-04 00:01:55 -0400

    feature: support services built from a Dockerfile

---
+++ test/build_service.bats
@@ -0,0 +1,24 @@
+#!/usr/bin/env bats
+
+load 'test_helper/debug'
+load 'test_helper/bats-support/load'
+load 'test_helper/bats-assert/load'
+load 'test_helper/workspace'
+
+BUILD_SERVICE_DIR="${PWD}/test/test_yamls/build"
+
+function setup() {
+	add_dir_with_ws ${BUILD_SERVICE_DIR} tarantino/build_service.yml
+}
+
+function teardown() {
+	tt destroy
+	rm_all_workspaces
+}
+
+@test "build service - it should build image and run container" {
+	debug_header
+	run tt workspace use build_service
+	run tt dc run hello
+	assert_output --regexp "Hello World!"
+}
--- test/test_helper/workspace.bash
+++ test/test_helper/workspace.bash
@@ -9,10 +9,23 @@ function add_new_workspace() {
 	echo "${TMP_DIR}"
 }
 
+function add_dir_with_ws() {
+	TEST_DIR="$1"
+	TMP_DIR=$(mktemp -d)
+	cp -r "$TEST_DIR"/* "$TMP_DIR"
+
+	YAML="$TMP_DIR"/"$2"
+	WS_DIR=$(dirname "$YAML")
+	WS_NAME=$(basename $YAML .yml)
+	mv "$YAML" "$WS_DIR"/docker-compose.yml
+	tt workspace add "$WS_NAME" "$WS_DIR"
+}
+
 function add_ws_from_yaml() {
 	YAML=$1
 	WS_NAME=$(basename $YAML .yml)
 	TMP_DIR=$(mktemp -d)
+	cp -r "$(dirname $YAML)"/* "$TMP_DIR"
 	ln -s "${YAML}" "${TMP_DIR}/docker-compose.yml"
 	tt workspace add "${WS_NAME}" "${TMP_DIR}"
 	echo "${TMP_DIR}"
